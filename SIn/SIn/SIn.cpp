61a6711de276bcd7617c8669462a59502e6c2829
DAShumilin <da.shumilin03@gmail.com>
2021-10-18 12:27:15 +0300

    SIn

---
+++ SIn/SIn/SIn.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+double sin(double x) {
+
+	double res = x;
+	double c = x;
+	double n = 1;
+
+	do {
+
+		c *= ((-1.0 * x * x) / ((2 * n + 3) * (2 * n + 2)));
+		res += c;
+		n++;
+		c = c < 0 ? -1 * c : c;
+
+	} while (c >= 0.001);
+
+	return res;
+
+}
+
+int main() {
+
+	double answer;
+
+	answer = sin(0.5);
+
+	printf("%lf", answer);
+
+	return 0;
+}
